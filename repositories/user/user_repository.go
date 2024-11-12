b4a466678fdd937393f69b3877686e3ac4f3cba4
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-12 14:00:21 +0700

    feat: implement user repository interface

---
+++ repositories/user/user_repository.go
@@ -0,0 +1,12 @@
+package user_repository
+
+import (
+	"user-service/models/user"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	CreateUser(ctx *fiber.Ctx, db *gorm.DB, useReq *user.CreateUserRequest) (string, error)
+}
