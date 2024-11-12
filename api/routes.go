2aa3b7c556cbf911a5e136df9c9d92cca40376f9
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-12 13:58:04 +0700

    refactor: update router

--- api/routes.go
+++ api/routes.go
@@ -1,17 +1,17 @@
 package api
 
 import (
-	user_controller "social-media/controllers/user"
+	user_controller "user-service/controllers/user"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func Router() {
+func Router(userController user_controller.UserController) *fiber.App {
 	app := fiber.New()
 	api := app.Group("/api")
 
 	signup := api.Group("/signup")
-	signup.Post("/user", user_controller.CreateUser)
+	signup.Post("/user", userController.CreateUser)
 
-	app.Listen(":3000")
+	return app
 }
