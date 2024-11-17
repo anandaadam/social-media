472ef8611c0601d9cd79e4a17ed59bb6563f6fb4
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-17 12:11:51 +0700

    refactor: update get kafka connection

--- config/kafka.go
+++ config/kafka.go
@@ -1,9 +1,14 @@
 package config
 
-import "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+import (
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
 
 func KafkaConfig() *kafka.ConfigMap {
 	return &kafka.ConfigMap{
-		"bootstrap.servers": "localhost:9092",
+		"bootstrap.servers": fmt.Sprintf("%s:%s", os.Getenv("KAFKA_HOST"), os.Getenv("KAFKA_PORT")),
 	}
 }
