a90adc36866b861817bc92e2f5ce96db8c7a7abe
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-17 17:41:59 +0700

    refactor: update user model

--- models/user/user.go
+++ models/user/user.go
@@ -3,11 +3,22 @@ package user
 import "time"
 
 type User struct {
-	Id        int64
-	Email     string
-	Password  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id             int64      `gorm:"column:id;primaryKey;autoIncrement"`
+	Email          string     `gorm:"column:email;unique;not null"`
+	Password       string     `gorm:"column:password;not null"`
+	Username       string     `gorm:"column:username;unique;not null"`
+	Name           *string    `gorm:"column:name"`
+	Phone          *string    `gorm:"column:phone"`
+	Birthdate      *time.Time `gorm:"column:birthdate"`
+	Avatar         *string    `gorm:"column:avatar"`
+	Biodata        *string    `gorm:"column:biodata"`
+	City           *string    `gorm:"column:city"`
+	Country        *string    `gorm:"column:country"`
+	FollowersTotal int        `gorm:"column:followers_total;default:0"`
+	FollowingTotal int        `gorm:"column:following_total;default:0"`
+	PrivateAccount bool       `gorm:"column:private_account;default:false"`
+	CreatedAt      time.Time  `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt      time.Time  `gorm:"column:updated_at;autoUpdateTime"`
 }
 
 func (User) TableName() string {
